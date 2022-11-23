191270c12c342c33cb633c6db850fe0a72cb05fe
Joao Bosco <jboscos.carvalho@gmail.com>
2022-11-23 18:17:47 -0300

    feat: Route Login estabelecida

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from "../pages/Login";
-import SignIn from "../pages/SignUp";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 }
